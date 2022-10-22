00ab15c252f2fc2184e9550b0d49fd86b232dcd9
kenj-i <raphael.gisler@stud.edubs.ch>
2022-10-22 14:29:49 +0200

    Bottom left texts are centered

--- src/Frame.java
+++ src/Frame.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
 
-public class Frame {
+public class Frame extends JFrame {
     private final JButton but1 = new JButton();
     private final JButton but2 = new JButton();
     private final JButton but3 = new JButton();
@@ -14,17 +14,15 @@ public class Frame {
     JButton[] buts = new JButton[]{but1, but2, but3, but4, but5, but6, but7, but8, but9};
     JButton[][] grid = new JButton[][] {{but1, but2, but3}, {but4, but5, but6}, {but7, but8, but9}, {but1, but4, but7}, {but2, but5, but8}, {but3, but6, but9}, {but1, but5, but9}, {but3, but5, but7}};
     JButton reset = new JButton("Restart Game");
-    JLabel message = new JLabel("Let's play a game!");
-    JLabel playmessage = new JLabel("Player 1, it's your turn");
+    JLabel message = new JLabel("Let's play a game!", SwingConstants.CENTER);
+    JLabel playmessage = new JLabel("Player 1, it's your turn", SwingConstants.CENTER);
 
     public Frame() {
-        JFrame frame = new JFrame();
         JPanel panel = new JPanel();
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
-        frame.setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
         panel.setLayout(new GridLayout(4, 3, 8, 8));
-        frame.add(panel);
+        add(panel);
         for (int i = 0; i < 9; i++) {
             panel.add(buts[i]);
         }
@@ -32,5 +30,6 @@ public class Frame {
         panel.add(message);
         panel.add(reset);
         reset.setVisible(false);
+        setVisible(true);
     }
 }
